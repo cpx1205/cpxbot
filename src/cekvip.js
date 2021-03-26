c77339fcaad97fde1f9ec1403a9ba12898dff821
cpxbot <srxdrongmer@gmail.com>
2021-03-26 20:15:37 -0300

    First commit

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  𝐂𝐏𝐗 𝐓𝐇𝐄 𝐎𝐅
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* 🐊🚩`
+}
+exports.cekvip = cekvip
